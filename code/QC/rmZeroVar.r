2bb0fa8335c0b1638a8b41aff80b619a613795a4
Kuangyu Wang <wangw1023@gmail.com>
2014-02-11 12:21:10 -0500

    rm correct zero var now

--- code/QC/rmZeroVar.r
+++ code/QC/rmZeroVar.r
@@ -1,32 +1,29 @@
-library(data.table)
-rawData = fread("train_v2.csv")
+rawData = read.csv("train_v2.csv")
 #total 771 vars
 colVar = apply(rawData, 2, var)
 zeroVarCols = names(which(colVar < 1e-8))
-#zero variance (19):
-#f33  f34  f35  f37  f38 f336 f382 f418 f465 f477 f480 f530 f659 f700 f701 f702 f732 f736 f764
+#zero variance (10):
+# [1] "f33"  "f34"  "f35"  "f37"  "f38"  "f700" "f701" "f702" "f736" "f764"
 
-for(i in zeroVarCols){
-  rawData[,paste0(i):=NULL]
-}
-#752 vars left
+rawData = rawData[,!(names(rawData) %in% zeroVarCols)]
+
+#761 vars left
 
 library(Hmisc)
 rawM = as.matrix(rawData)
 #drop "id" and "loss" column
-rawM = rawM[,-c(1,752)]
+rawM = rawM[,-c(1,761)]
 corrM = rcorr(rawM)$r
 
 perfectCorrEntries = which(corrM > 1-1e-6, arr.in=TRUE)
 colDiffVec = perfectCorrEntries[,2] - perfectCorrEntries[,1]
 perfectCorrEntries = cbind(perfectCorrEntries, colDiffVec)
-perfectCorrPairs = pecrfectCorrEntries[which(perfectCorrEntries[,3]>0),]
+perfectCorrPairs = perfectCorrEntries[which(perfectCorrEntries[,3]>0),]
 uniqColNrs = unique(perfectCorrPairs[,2])
 colNames = colnames(corrM)
 col2rm = colNames[uniqColNrs]
 
 #vars to be removed:
-
 #  [1] "f58"  "f86"  "f87"  "f88"  "f96"  "f97"  "f98"  "f106" "f107" "f108"
 # [11] "f116" "f117" "f118" "f126" "f127" "f128" "f155" "f156" "f157" "f165"
 # [21] "f166" "f167" "f175" "f176" "f177" "f185" "f186" "f187" "f195" "f196"
@@ -39,8 +36,8 @@ col2rm = colNames[uniqColNrs]
 # [91] "f548" "f549" "f553" "f558" "f559" "f563" "f568" "f569" "f573" "f577"
 #[101] "f578" "f582" "f599" "f722" "f729" "f741" "f763" "f765" "f768" "f770"
 
-for(i in col2rm){
-    rawData[,paste0(i):=NULL]
-}
+rawData = rawData[,!(names(rawData) %in% col2rm)]
+rawData$binaryLoss = rep(0, nrow(rawData))
+rawData$binaryLoss[which(rawData$loss > 0)] = 1
 
 write.table(rawData,file="train_v2_filtered.csv",sep=",",row.names=F)
